8d0df9531692de4e5a63ffd660a69d01c3677baa
Steven Wilson <2165238+mendeni@users.noreply.github.com>
2026-01-07 17:13:33 -0500

    Build for both x86_64 and arm64 on macOS (Darwin)

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -x
 
-./configure CC="gcc -arch x86_64 -arch arm64" --enable-static --disable-shared --disable-doc
+# build for both macOS archs
+if [ `uname -o` == 'Darwin' ]; then
+  export CFLAGS="-arch x86_64 -arch arm64 -mmacosx-version-min=10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path)"
+fi
 
+make clean
+./configure --enable-static --disable-shared --disable-doc
 make
-
-ls -l ./src/.libs/liblo.a
